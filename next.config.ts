d79244d3a7ae1de32bb621a84abdd9bd260e21fe
Nick Roofthooft <nroofthooft2012@gmail.com>
2025-08-31 12:56:46 +0300

    Updated sitemap

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,21 @@
 // next-sitemap.config.js
-const pages = ["about", "contact", "faq", "review", "services-prices", ""];
-const langs = ["est", "en"];
+// const pages = ["about", "contact", "faq", "review", "services-prices", ""];
+// const langs = ["est", "en"];
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://www.beautybystiina.ee",
   generateRobotsTxt: true,
   sitemapSize: 5000,
-  exclude: ["/*"], // prevent auto-detection
-  // @ts-expect-error rerre
-  additionalPaths: async (config) => {
-    const paths = [];
-    for (const lang of langs) {
-      for (const page of pages) {
-        paths.push(await config.transform(config, `/${lang}/${page}/`));
-      }
-    }
-    return paths;
-  },
+  // exclude: ["/*"], // prevent auto-detection
+
+  // additionalPaths: async (config) => {
+  //   const paths = [];
+  //   for (const lang of langs) {
+  //     for (const page of pages) {
+  //       paths.push(await config.transform(config, `/${lang}/${page}/`));
+  //     }
+  //   }
+  //   return paths;
+  // },
 };
