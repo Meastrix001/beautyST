faaba8820b9eb66db1d508e6ba1299169f110149
Nick Roofthooft <nroofthooft2012@gmail.com>
2025-09-03 09:21:54 +0300

    Added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,11 @@
 import "@radix-ui/themes/styles.css";
 import "@/styles/main.scss";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Box, Flex, Theme } from "@radix-ui/themes";
 import { Footer, Navbar } from "@/components";
 import I18nProvider from "@/providers/I18nProvider";
-import type { Metadata } from "next";
 import { brand } from "@/theme/brand.config";
 import { BuildSiteMap } from "@/utils/sitemap/sitemap.builder";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(brand.company.site), // ✅ important for resolving OG/Twitter images
-
-  alternates: {
-    canonical: "layout"
-  },
-  title: brand.company.name,
-  description: "Get full body waxed in Tallinn, Estonia",
-  openGraph: {
-    title: brand.company.name,
-    description: "Get full body waxed in Tallinn, Estonia",
-    url: `${brand.company.site}`,
-    siteName: brand.company.name,
-    images: [
-      {
-        url: brand.company.logo.src,
-        width: 1200,
-        height: 630,
-        alt: `${brand.company.name} hero image`,
-      },
-    ],
-    type: "website",
-  },
-};
 
 BuildSiteMap()
 export default function RootLayout({
@@ -50,6 +25,7 @@ export default function RootLayout({
                 <Navbar />
                 <Box flexGrow="1">{children}</Box>
                 <Footer />
+                <SpeedInsights />
               </Flex>
             </Box>
           </Theme>
