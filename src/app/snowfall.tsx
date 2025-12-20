5cde904a2996ccff8152498299342553c3a1469b
Nick Roofthooft <nroofthooft2012@gmail.com>
2025-12-20 22:31:13 +0200

    Added snowflake svg as snowflake item

--- src/app/snowfall.tsx
+++ src/app/snowfall.tsx
@@ -1,9 +1,13 @@
 "use client"
 import { Snowfall } from 'react-snowfall';
+const snowflake1 = document.createElement('img')
+snowflake1.src = '/static/snowflake.svg'
+
+const images = [snowflake1]
 
 
 
 
 export const SnowFall = () => {
-    return <Snowfall color='white' snowflakeCount={300} style={{ zIndex: 1000 }} />
+    return <Snowfall color='white' radius={[0.5, 20]} images={images} snowflakeCount={500} style={{ zIndex: 1000 }} />
 }
